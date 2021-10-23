6a119d455bd58a979169ebcbbe12e8901317811b
TomascpMarques <tomascpmarques@gmail.com>
2021-10-23 23:56:27 +0100

    Remoção do arg_help.py file, mas não do argumento

--- src/cli/arg_resolvers/file_resolver.py
+++ src/cli/arg_resolvers/file_resolver.py
@@ -7,7 +7,7 @@ import re
 from cli.arg_resolvers.arg_setup import CliArgumento
 
 
-def run_arg_file(path_ficheiro: str):
+def run_arg_file(path_ficheiro: str) -> None:
     """
     some some here
     """
@@ -19,11 +19,17 @@ def run_arg_file(path_ficheiro: str):
         print(f'{ficheiro.read()=}')
 
 
-file: CliArgumento = CliArgumento(
+file_mens_ajuda: str = \
+    '* Parametro: <file> | Exemplo: --file ./alguma/pasta/ficheiro.txt\n\
+O argumento têm que apontar para um ficheiro/diretoria válida'
+
+file_arg: CliArgumento = CliArgumento(
     chave='file',
     run=run_arg_file,
     descricao_argumento='O ficheiro de template a transpilar',
     erro_validacao='Não foi possivél validar o valor para o argumento <file>',
+    mensagem_ajuda=file_mens_ajuda,
     re_validacao_tipo_valor=re.compile(
-        r'(\.\/|\/|~\/)(\w+\/)+\w+\.\w+|\.\/\w+\.\w+'),
+        r'(\.\/|\/|~\/)(\w+\/)+\w+\.\w+|\.\/\w+\.\w+'
+    ),
 )
