c77fc135a2f5d31fc0fb5e85d5b7a9a9dfc93f58
TomascpMarques <tomascpmarques@gmail.com>
2021-10-25 10:46:58 +0100

    O file hadler agora lida com pastas tambem. Code refactors

--- src/file_handeling/handler.py
+++ src/file_handeling/handler.py
@@ -22,16 +22,23 @@ FILE_EXTENSIONS: list[str] = ['.htt', '.httconfig']
 
 class FileHandler:
     """
-    Handles file rading/writting
+    Handles file rading/writting, também ficheiros dentro de pastas
     """
 
     def __init__(self, caminho: str):
+        self.caminho: str = caminho
+
         self.ficheiros: dict[str, os.DirEntry] = dict(
             (entrada.name, entrada) for entrada in os.scandir(caminho)
-            if entrada.is_file()
-            and entrada.name[entrada.name.index('.'):]
-            in FILE_EXTENSIONS
+            if (
+                entrada.is_file()
+                and entrada.name[entrada.name.index('.'):]
+                in FILE_EXTENSIONS
+            ) or entrada.is_dir()
         )
+
+        for _, entry in self.ficheiros.items():
+            print(f'{entry.path}')
 
         if not self.ficheiros.__len__():
             erro_exit(
@@ -40,7 +47,6 @@ class FileHandler:
                 tipo_erro='NotEnoughFiles'
             )
 
-        self.caminho: str = caminho
         print(self.ficheiros)
 
     def nome_ficheiros(self) -> list[str]:
@@ -52,34 +58,35 @@ class FileHandler:
         """
         return self.ficheiros.keys()
 
-    def resolver_ficheiro(self, nome: str) -> list[str]:
+    def resolver_ficheiro(self, caminho: str) -> list[str]:
         """
         Devolve o conteudo do ficheiro pedido
 
         Args:
-            nome (str): Nome do ficheiro a ler o conteudo do ficheiro
+            caminho (str): Path do ficheiro a ler o conteudo
 
         Returns:
             list[str]: Conteudos do ficheiro em linhas
         """
+        print(f'{self.ficheiros.get(caminho).path}')
         with open(
-            os.path.join(self.caminho, nome),
+            os.path.join(self.caminho, caminho),
             'r', encoding=sys.getfilesystemencoding()
         ) as ficheiro:
             return list(map(lambda x: x.replace('\n', ''), ficheiro.readlines()))
 
-    def ficheiro_dir_entry(self, nome: str) -> os.DirEntry | None:
+    def ficheiro_dir_entry(self, caminho: str) -> os.DirEntry | None:
         """
         Devolve a dir entry do ficheiro pedidos
 
         Returns:
             os.DirEntry | None: A dir entry especificada ou None
         """
-        if nome not in self.ficheiros.keys():
+        if caminho not in self.ficheiros.keys():
             return None
-        return self.ficheiros.get(nome)
+        return self.ficheiros.get(caminho)
 
-    def ficheiros_dir_entry(self, *nomes: str) -> list[os.DirEntry]:
+    def ficheiros_dir_entry(self, *caminhos: str) -> list[os.DirEntry]:
         """
         Devolve as dir entrys dos ficheiro pedidos
 
@@ -87,13 +94,13 @@ class FileHandler:
             list[os.DirEntry]: Lista das dir entries dso ficheiros pedidos
         """
         lista_ficheiros: list[os.DirEntry] = []
-        for nome in nomes:
+        for nome in caminhos:
             if nome not in self.ficheiros.keys():
                 return []
             lista_ficheiros.append(self.ficheiros.get(nome))
         return lista_ficheiros
 
-    def dump_ficheiros(self, nomes: list[str]) -> list[list[str]]:
+    def dump_ficheiros(self, *caminhos: str) -> list[list[str]]:
         """
         Devolve o conteudo dos ficheiros pedidos
 
@@ -103,16 +110,27 @@ class FileHandler:
         Returns:
             list[list[str]]: O conteudo dos ficheiros, separados por linhas
         """
-        lista_conteudo: list[list[str]] = [[]]
-        for nome in nomes:
-            with open(
-                os.path.join(self.caminho, nome),
-                'r', encoding=sys.getfilesystemencoding()
-            ) as ficheiro:
-                lista_conteudo.append(
-                    [map(
-                        lambda x: x.replace('\n', ''),
-                        ficheiro.readlines()
-                    )]
+        lista_conteudo: list[list[str]] = []
+        for nome in caminhos:
+            try:
+                with open(
+                    os.path.join(self.caminho, nome),
+                    'r',
+                    encoding=sys.getfilesystemencoding()
+                ) as file:
+                    lista_conteudo.append(
+                        file.readlines()
+                    )
+            except FileNotFoundError:
+                erro_exit(
+                    time_stamp=True,
+                    tipo_erro='FileNotFoundError',
+                    menssagen=f'O ficheiro <{nome}> não existe'
+                )
+            except IsADirectoryError:
+                erro_exit(
+                    time_stamp=True,
+                    tipo_erro='IsADirectoryError',
+                    menssagen=f'O ficheiro <{nome}> é uma diretoria, não um ficheiro'
                 )
         return lista_conteudo
