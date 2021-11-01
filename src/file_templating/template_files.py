1886339a8a792f307db0cb0c0a47502310b77de2
TomascpMarques <tomascpmarques@gmail.com>
2021-11-01 21:43:29 +0000

    Desenvolvimento da class TemplatingFiles: Agora lê o conteudo .htt existente na dir, e parse os ficheiros. Desenvolvimento da class TemplateFile: Atribuição automática dos parametros do ficheiro htt para a class em sí.

--- src/file_templating/template_files.py
+++ src/file_templating/template_files.py
@@ -3,17 +3,9 @@
     Ler os mesmos, e interpretalos
 """
 
+from dataclasses import dataclass
 
-from file_handeling.handler import FileHandler
-
-
-class TemplateFile:
-    """
-    Some Some
-    """
-
-    def __init__(self):
-        pass
+from file_handeling.handler import FileHandler, parse_htt_file
 
 
 class TemplatingFiles(FileHandler):
@@ -26,3 +18,55 @@ class TemplatingFiles(FileHandler):
         super().__init__(path)
 
         self.conteudo_dir = super().conteudo_dir()
+        self.htt_templates: dict[str, any] = {}
+
+        for key, dir_entry in self.conteudo_dir.items():
+            if '.httconfig' not in key and dir_entry.is_file():
+                self.htt_templates.setdefault(
+                    key,
+                    TemplateFile(
+                        parse_htt_file(
+                            super().
+                            resolver_conteudo_ficheiro(dir_entry.path)
+                        )
+                    ),
+                )
+
+
+@dataclass
+class TemplateFile:
+    """
+    Some Some
+    """
+
+    def __init__(self, file_tags: dict[str, list[str]]):
+        for key, valor in file_tags.items():
+            self.__setattr__(key, valor)
+
+    def keys(self) -> list[str]:
+        """
+        Retorna as keys/valores da class
+
+        Returns:
+            list[str]: Lista dos valores existentes da class
+        """
+        return list(
+            map(
+                lambda x: x.replace('__', ''),
+                self.__dict__
+            )
+        )
+
+    def keys_por_tipo(self, tipo: str) -> list[str]:
+        """
+        Devolve todas as keys semelhantes ao tipo especificado
+
+        Args:
+            tipo (str): Tipo da key a agrupar
+
+        Returns:
+            list[str]: Lista das keys semelhantes ao tipo dado
+        """
+        return list(
+            key for key in self.keys() if key.endswith(tipo)
+        )
