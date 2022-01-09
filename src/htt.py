8105b7c6e9d75253a29367f918f34eddac3062ca
TomascpMarques <tomascpmarques@gmail.com>
2022-01-09 18:08:47 +0000

    Relative file paths, and app exec

--- src/htt.py
+++ src/htt.py
@@ -2,10 +2,12 @@
 O ponto inicial do programa
 """
 
+import os
 import sys
 
 from cli.cli import Cli
 from cli_args.args import CLI_ARGS
+from cli_store.store import cli_store_set
 
 
 def main():
@@ -13,6 +15,7 @@ def main():
     Função a correr se o ficheiro
     for invocado como um script.
     """
+    cli_store_set('base_path', os.getcwd())
     program_cli = Cli(sys.argv, **CLI_ARGS)
     program_cli.run()
 
